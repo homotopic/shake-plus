b25a405f443b155c4e4664b19585d793b2b86eef
Daniel Firth <locallycompact@gmail.com>
2020-05-05 15:26:09 +0000

    module reexports

--- src/Development/Shake/Plus.hs
+++ src/Development/Shake/Plus.hs
@@ -1,5 +1,15 @@
 module Development.Shake.Plus (
+  module Development.Shake.Plus.Cache
   module Development.Shake.Plus.Core
+, module Development.Shake.Plus.Directory
+, module Development.Shake.Plus.File
+, module Development.Shake.Plus.FileRules
+, module Development.Shake.Plus.Orcle
 ) where
 
+import Development.Shake.Plus.Cache
 import Development.Shake.Plus.Core
+import Development.Shake.Plus.File
+import Development.Shake.Plus.FileRules
+import Development.Shake.Plus.Directory
+import Development.Shake.Plus.Oracle
